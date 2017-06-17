807b27ea15a6dcd1e5fe3630d5e1f2a3180c46c8
paopao74cn <zhangqian06@gmail.com>
2017-06-17 10:17:59 -0400

    update after debug

--- make.sh
+++ make.sh
@@ -17,38 +17,32 @@ $YW_CMD graph
 #         -c model.factsfile=$FACTS_DIR/yw_model_facts.P \
 #         -c query.engine=xsb
 
-# materialize views of YW facts
-$QUERIES_DIR/materialize_yw_views.sh > $VIEWS_DIR/yw_views.P
-
-# run the workflow
-java -jar target/kurator-validation-1.0.1-SNAPSHOT-jar-with-dependencies.jar -f packages/kurator_branching/workflows/file_branching_taxon_lookup.yaml -p inputfile=packages/kurator_branching/data/kurator_sample_data.txt -l ALL > runlog.log 2>&1
-
-# generate reconfacts.P to facts/ folder 
-
-# cd packages/kurator_branching/workflows/
-# java -jar ../../../yw_jar/yesworkflow-0.2.2.0-alpha-jar-with-dependencies.jar recon file_branching_taxon_lookup.yaml \
-#         -c extract.language=bash \
-#         -c recon.factsfile=facts/reconfacts.P \
-#         -c query.engine=xsb
-# cd ../../../
-$YW_CMD recon
 # draw complete workflow graph with URI template
-$YW_CMD graph $WORKFLOWS_DIR/file_branching_taxon_lookup.yaml \
-		-c extract.language=bash \
-        -c graph.view=combined \
-        -c graph.layout=tb \
-        > $RESULTS_DIR/complete_wf_graph_uri.gv
+# $YW_CMD graph $WORKFLOWS_DIR/file_branching_taxon_lookup.yaml \
+#   	  -c extract.language=bash \
+#         -c graph.view=combined \
+#         -c graph.layout=tb \
+#         > $RESULTS_DIR/complete_wf_graph_uri.gv
 dot -Tpdf $RESULTS_DIR/complete_wf_graph_uri.gv > $RESULTS_DIR/complete_wf_graph_uri.pdf
 dot -Tsvg $RESULTS_DIR/complete_wf_graph_uri.gv > $RESULTS_DIR/complete_wf_graph_uri.svg
 
 
 # draw complete workflow graph
-$QUERIES_DIR/render_complete_wf_graph.sh > $RESULTS_DIR/complete_wf_graph.gv
-dot -Tpdf $RESULTS_DIR/complete_wf_graph.gv > $RESULTS_DIR/complete_wf_graph.pdf
-dot -Tsvg $RESULTS_DIR/complete_wf_graph.gv > $RESULTS_DIR/complete_wf_graph.svg
+# $QUERIES_DIR/render_complete_wf_graph.sh > $RESULTS_DIR/complete_wf_graph.gv
+# dot -Tpdf $RESULTS_DIR/complete_wf_graph.gv > $RESULTS_DIR/complete_wf_graph.pdf
+# dot -Tsvg $RESULTS_DIR/complete_wf_graph.gv > $RESULTS_DIR/complete_wf_graph.svg
+
+# materialize views of YW facts
+$QUERIES_DIR/materialize_yw_views.sh > $VIEWS_DIR/yw_views.P
 
 # list workflow outputs
 $QUERIES_DIR/list_workflow_outputs.sh > $RESULTS_DIR/workflow_outputs.txt
+
+# run the workflow
+java -jar target/kurator-validation-1.0.1-SNAPSHOT-jar-with-dependencies.jar -f packages/kurator_branching/workflows/file_branching_taxon_lookup.yaml -p inputfile=packages/kurator_branching/data/kurator_sample_data_v2.txt -l ALL > runlog.log 2>&1
+
+# generate reconfacts.P to facts/ folder 
+$YW_CMD recon
 
 
 ##############
@@ -56,24 +50,28 @@ $QUERIES_DIR/list_workflow_outputs.sh > $RESULTS_DIR/workflow_outputs.txt
 ##############
 
 # draw worfklow graph upstream of OutputFile
-$QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'OutputFile\' > $RESULTS_DIR/wf_upstream_of_OutputFile.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_OutputFile.gv > $RESULTS_DIR/wf_upstream_of_OutputFile.pdf
-dot -Tsvg $RESULTS_DIR/wf_upstream_of_OutputFile.gv > $RESULTS_DIR/wf_upstream_of_OutputFile.svg
+productName=OutputFile
+$QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'$productName\' > $RESULTS_DIR/wf_upstream_of_$productName.gv
+dot -Tpdf $RESULTS_DIR/wf_upstream_of_$productName.gv > $RESULTS_DIR/wf_upstream_of_$productName.pdf
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_$productName.gv > $RESULTS_DIR/wf_upstream_of_$productName.svg
 
-# draw worfklow graph upstream of MergedStream
-$QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'MergedStream\' > $RESULTS_DIR/wf_upstream_of_MergedStream.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_MergedStream.gv > $RESULTS_DIR/wf_upstream_of_MergedStream.pdf
-dot -Tsvg $RESULTS_DIR/wf_upstream_of_MergedStream.gv > $RESULTS_DIR/wf_upstream_of_MergedStream.svg
+# draw worfklow graph upstream of GBIFOutput
+productName="GBIFOutput"
+$QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'$productName\' > $RESULTS_DIR/wf_upstream_of_$productName.gv
+dot -Tpdf $RESULTS_DIR/wf_upstream_of_$productName.gv > $RESULTS_DIR/wf_upstream_of_$productName.pdf
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_$productName.gv > $RESULTS_DIR/wf_upstream_of_$productName.svg
 
 # draw worfklow graph upstream of LogFile
-$QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'LogFile\' > $RESULTS_DIR/wf_upstream_of_LogFile.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_LogFile.gv > $RESULTS_DIR/wf_upstream_of_LogFile.pdf
-dot -Tsvg $RESULTS_DIR/wf_upstream_of_LogFile.gv > $RESULTS_DIR/wf_upstream_of_LogFile.svg
+productName="LogFile"
+$QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'$productName\' > $RESULTS_DIR/wf_upstream_of_$productName.gv
+dot -Tpdf $RESULTS_DIR/wf_upstream_of_$productName.gv > $RESULTS_DIR/wf_upstream_of_$productName.pdf
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_$productName.gv > $RESULTS_DIR/wf_upstream_of_$productName.svg
 
-# draw worfklow graph upstream of Marine
-$QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'Marine\' > $RESULTS_DIR/wf_upstream_of_Marine.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_Marine.gv > $RESULTS_DIR/wf_upstream_of_Marine.pdf
-dot -Tsvg $RESULTS_DIR/wf_upstream_of_Marine.gv > $RESULTS_DIR/wf_upstream_of_Marine.svg
+# draw worfklow graph upstream of <MarineDataRecord[scientificName:scientificNameAuthorship]>
+productName="<MarineDataRecord[scientificName:scientificNameAuthorship]>"
+$QUERIES_DIR/render_wf_graph_upstream_of_data_q1.sh \'$productName\' > $RESULTS_DIR/wf_upstream_of_$productName.gv
+dot -Tpdf $RESULTS_DIR/wf_upstream_of_$productName.gv > $RESULTS_DIR/wf_upstream_of_$productName.pdf
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_$productName.gv > $RESULTS_DIR/wf_upstream_of_$productName.svg
 
 
 ##############
@@ -110,13 +108,47 @@ $QUERIES_DIR/list_outputs_downstream_of_data_q4.sh \'DataRecord\' DataRecord > $
 # list workflow outputs downstream of Workspace
 $QUERIES_DIR/list_outputs_downstream_of_data_q4.sh \'Workspace\' Workspace > $RESULTS_DIR/outputs_downstream_of_Workspace.txt
 
+
+##############
+#   Q5_pro   #
+##############
+
+# draw recon worfklow graph upstream of OutputFile
+productName="OutputFile"
+$QUERIES_DIR/render_wf_recon_graph_upstream_of_data_q5.sh \'$productName\' > $RESULTS_DIR/wf_recon_upstream_of_$productName.gv
+dot -Tpdf $RESULTS_DIR/wf_recon_upstream_of_$productName.gv > $RESULTS_DIR/wf_recon_upstream_of_$productName.pdf
+dot -Tsvg $RESULTS_DIR/wf_recon_upstream_of_$productName.gv > $RESULTS_DIR/wf_recon_upstream_of_$productName.svg
+
+# draw recon worfklow graph upstream of LogFile
+productName="LogFile"
+$QUERIES_DIR/render_wf_recon_graph_upstream_of_data_q5.sh \'$productName\' > $RESULTS_DIR/wf_recon_upstream_of_$productName.gv
+dot -Tpdf $RESULTS_DIR/wf_recon_upstream_of_$productName.gv > $RESULTS_DIR/wf_recon_upstream_of_$productName.pdf
+dot -Tsvg $RESULTS_DIR/wf_recon_upstream_of_$productName.gv > $RESULTS_DIR/wf_recon_upstream_of_$productName.svg
+
+
 ##############
 #   Q6_pro   #
 ##############
 
-
-# draw recon workflow graph with all observables
-
+# draw recon workflow graph with all (file-level) runtime observables
 $QUERIES_DIR/render_recon_complete_wf_graph_q6.sh > $RESULTS_DIR/wf_recon_complete_graph_all_observables.gv
 dot -Tpdf $RESULTS_DIR/wf_recon_complete_graph_all_observables.gv > $RESULTS_DIR/wf_recon_complete_graph_all_observables.pdf
 dot -Tsvg $RESULTS_DIR/wf_recon_complete_graph_all_observables.gv > $RESULTS_DIR/wf_recon_complete_graph_all_observables.svg
+
+##############
+#   Q7_pro   #
+##############
+
+# given an occurrenceID, draw hybrid complete provenance graph with record-level runtime observables
+# isMarine occurrenceID="MCZ:Mala:303063"
+occurrenceID="MCZ:Mala:303063"
+$QUERIES_DIR/render_recon_complete_wf_graph_record_level_q7.sh \'$occurrenceID\' > $RESULTS_DIR/wf_recon_complete_graph_of_$occurrenceID.gv
+dot -Tpdf $RESULTS_DIR/wf_recon_complete_graph_of_$occurrenceID.gv > $RESULTS_DIR/wf_recon_complete_graph_of_$occurrenceID.pdf
+dot -Tsvg $RESULTS_DIR/wf_recon_complete_graph_of_$occurrenceID.gv > $RESULTS_DIR/wf_recon_complete_graph_of_$occurrenceID.svg
+
+# noMarine occurrenceID="MCZ:Mala:278687"
+occurrenceID="MCZ:Mala:278687"
+$QUERIES_DIR/render_recon_complete_wf_graph_record_level_q7.sh \'$occurrenceID\' > $RESULTS_DIR/wf_recon_complete_graph_of_$occurrenceID.gv
+dot -Tpdf $RESULTS_DIR/wf_recon_complete_graph_of_$occurrenceID.gv > $RESULTS_DIR/wf_recon_complete_graph_of_$occurrenceID.pdf
+dot -Tsvg $RESULTS_DIR/wf_recon_complete_graph_of_$occurrenceID.gv > $RESULTS_DIR/wf_recon_complete_graph_of_$occurrenceID.svg
+ 
